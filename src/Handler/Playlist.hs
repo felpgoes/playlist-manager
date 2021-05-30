efbf3e40e0839a2efdb14433cb3ad54dc7acdb26
felpgoes <felipe.goess@hotmail.com>
2021-05-30 20:16:00 -0300

    feat: adding playlist interactions

--- src/Handler/Playlist.hs
+++ src/Handler/Playlist.hs
@@ -12,19 +12,28 @@ import Text.Julius
 
 formPlaylist :: Form Playlist
 formPlaylist = renderDivs $ Playlist
-    <$> areq textField "Nome: "        Nothing
-    <*> areq textField "Descrição:  "  Nothing
-    <*> areq textField  "id: "          Nothing
+    <$> areq textField "Nome "        Nothing
+    <*> areq textField "Descrição  "  Nothing
+    <*> areq textField "Apelido "     Nothing
 
 getPlaylistR :: PlaylistId -> Handler Html
-getPlaylistR ident = do
+getPlaylistR playId = do
+    playlist <- runDB $ get404 playId
     defaultLayout $ do
-        setTitle "getPlaylistR"
-    -- playlist <- runDB $ get404 ident
-    -- defaultLayout $ do
-    --     addStylesheet (StaticR css_bootstrap_css)
-    --     toWidgetHead $(cassiusFile "templates/Padrao.cassius")
-    --     $(whamletFile "templates/Cliente.hamlet")
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(cassiusFile "templates/Padrao.cassius")
+        toWidgetHead $(cassiusFile "templates/Playlist.cassius")
+        $(whamletFile "templates/Playlist.hamlet")
+
+getPlaylistFormR :: Handler Html
+getPlaylistFormR = do   
+    (widget,_) <- generateFormPost formPlaylist
+    msg <- getMessage
+    defaultLayout $ do
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(cassiusFile "templates/Padrao.cassius")
+        toWidgetHead $(cassiusFile "templates/PlaylistForm.cassius")
+        $(whamletFile "templates/PlaylistForm.hamlet")
 
 postCriarPlaylistR :: Handler Html
 postCriarPlaylistR = do
@@ -34,17 +43,21 @@ postCriarPlaylistR = do
             runDB $ insert playlist
             setMessage [shamlet|
                 <div>
-                    A playlist #{playlistName playlist} foi criada!
+                    Playlist #{playlistName playlist} foi criada com sucesso!
             |]
             redirect ListPlaylistsR
         _ -> redirect HomeR
 
 postDelPlaylistR :: PlaylistId -> Handler Html
-postDelPlaylistR id = do
-    runDB $ delete id
-    redirect HomeR
+postDelPlaylistR playId = do
+    runDB $ delete playId
+    redirect ListPlaylistsR
 
 getListPlaylistsR :: Handler Html
 getListPlaylistsR = do
+    playlists <- runDB $ selectList [] [Desc PlaylistId]
     defaultLayout $ do
-        setTitle "getListPlaylistsR"
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(cassiusFile "templates/Padrao.cassius")
+        toWidgetHead $(cassiusFile "templates/PlaylistList.cassius")
+        $(whamletFile "templates/PlaylistList.hamlet")
